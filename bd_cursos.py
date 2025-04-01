937baab2deaa318b9edd1e4335d4dfbf76ef34fe
CibeleLucia <cibeleluciasha@gmail.com>
2025-03-31 22:08:26 -0300

    Cursos

---
+++ bd_cursos.py
@@ -0,0 +1,22 @@
+Cursos = [
+{
+    'id':'1',
+    'descricao': 'Ciência da Computação',
+    'duracao': '4 anos'
+},
+{
+    'id':'2',
+    'descricao': 'Engenharia de Software',
+    'duracao': '5 anos'
+},
+{
+    'id':'3',
+    'descricao': 'Análise e Desenvolvimento de Sistemas',
+    'duracao': '3 anos'
+},
+{
+    'id':'4',
+    'descricao': 'Gestão da Tecnologia da Informação',
+    'duracao': '2 anos' 
+}
+]
--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from flask import Flask, jsonify, request
-from bd_alunos import Alunos
+from bd import Alunos
+from bd_cursos import Cursos
 
 app = Flask(__name__)   
 
+#Listar todos os alunos
 @app.route('/Alunos', methods=['GET'])
 def get_alunos():
     return jsonify (Alunos)
 
+#Consultar um aluno pelo CPF
+@app.route('/Alunos/<string:cpf>', methods=['GET'])
+def consultar_aluno(cpf):
+    for aluno in Alunos:
+        if aluno.get('cpf') == cpf:
+            return jsonify(aluno)
     
-
+#Cadastrar um novo aluno
 @app.route('/Alunos', methods=['POST'])
 def create_novo_aluno():
     novo_aluno = request.get_json()
@@ -16,7 +24,20 @@ def create_novo_aluno():
     
     return jsonify (novo_aluno)
 
+#Listar todos os cursos
+@app.route('/Cursos', methods=['GET'])
+def get_cursos():
+    return jsonify (Cursos)
 
+
+#Consultar um curso pelo ID
+@app.route('/Cursos/<int:id>', methods=['GET'])
+def obter_curso_por_id(id):
+    for curso in Cursos: 
+        if curso.get('id') == id:
+            return jsonify(curso)
+            
+            
 
 
 
